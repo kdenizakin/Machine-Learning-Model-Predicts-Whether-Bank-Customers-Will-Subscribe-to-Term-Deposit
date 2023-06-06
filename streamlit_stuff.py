bf0913d3afa5e1fb3618c3760430cc6c705e9a47
Kerem0I <keremirmak@gmail.com>
2023-06-06 19:27:13 +0300

    Update streamlit_stuff.py

--- streamlit_stuff.py
+++ streamlit_stuff.py
@@ -8,7 +8,7 @@ import sklearn
 
 st.title('Deposit Prediction Web App')
 
-age = st.number_input("Enter an integer",  format="%d", key="age", type=int)
+age = st.number_input("Enter an integer",value=0, step=1,  format="%d", key="age", type=int)
 
 job = st.selectbox(
      'Please select your job',
