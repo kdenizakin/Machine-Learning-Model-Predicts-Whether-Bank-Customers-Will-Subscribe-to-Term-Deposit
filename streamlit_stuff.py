b7a840577998e7f0fed621cf461b4d05a0273bef
kdakn <109918200+kdakn@users.noreply.github.com>
2023-06-06 16:47:53 +0300

    c

--- streamlit_stuff.py
+++ streamlit_stuff.py
@@ -1,65 +1,66 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle #pickle for saving the model.
+
 
 st.title('My first Streamlit app')
-st.write('Hello, world!')
 
 age = st.number_input('Age:')
-st.write('The current number is ', age)
 
 job = st.text_input('Job:', )
-st.write('The current movie title is', job)
+
 
 marital = st.text_input('marital:', )
-st.write('The current movie title is', marital)
 
 education = st.text_input('education:', )
-st.write('The current movie title is', education)
 
 default = st.text_input('default:', )
-st.write('The current movie title is', default)
 
 housing = st.text_input('housing:', )
-st.write('The current movie title is', housing)
 
 loan = st.text_input('loan:', )
-st.write('The current movie title is', loan)
 
 contact = st.text_input('contact:', )
-st.write('The current movie title is', contact)
 
 month = st.text_input('month:', )
-st.write('The current movie title is', month)
 
 day_of_week = st.text_input('day_of_week:',)
-st.write('The current number is ', day_of_week)
 
 duration = st.number_input('duration:')
-st.write('The current number is ', duration)
 
 campaign = st.number_input('campaign:')
-st.write('The current number is ', campaign)
 
 pdays = st.number_input('pdays:')
-st.write('The current number is ', pdays)
-
 previous = st.number_input('previous:')
-st.write('The current number is ', previous)
 
 poutcome = st.text_input('poutcome:', )
-st.write('The current movie title is', poutcome)
 
 emp_var_rate = st.number_input('emp_var_rate:')
-st.write('The current number is ', emp_var_rate)
 
 cons_price_idx = st.number_input('cons_price_idx:')
-st.write('The current number is ', cons_price_idx)
 
 cons_conf_idx = st.number_input('cons_conf_idx:')
-st.write('The current number is ', cons_conf_idx)
 
 euribo3m = st.number_input('euribo3m:')
-st.write('The current number is ', euribo3m)
 
 nr_employed = st.number_input('nr_employed:')
-st.write('The current number is ', nr_employed)
+
+
+
+
+#Test single input.
+input_data = np.array([30, "blue-collar", "married",	"basic.9y",	"no",	"yes",	"no",	"cellular",	"may",	"fri",	487,	2,	999,	0,	"nonexistent",	-1.8,	92.893,	-46.2,	1.313,	5099.1])
+input_data = input_data.reshape(1, -1)
+columns = ['age', 'job', 'marital', 'education', 'default', 'housing', 'loan', 'contact', 'month', 'day_of_week', 'duration', 'campaign', 'pdays', 'previous', 'poutcome', 'emp.var.rate', 'cons.price.idx', 'cons.conf.idx', 'euribor3m', 'nr.employed']
+input_data_df = pd.DataFrame(input_data, columns=columns)
+
+print("hello")
+pickled_model = pickle.load(open("models/tuned_best_model.pkl", 'rb'))#Load the model.
+
+input_predictions = pickled_model.predict(input_data_df)
+
+# Print out the prediction
+print(input_predictions)
 
